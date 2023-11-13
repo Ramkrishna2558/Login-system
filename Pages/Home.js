1cc9dd2ec3e4b46e45db2df0b6fc259a6eb6873c
ram <ramkrishnamore299@gmail.com>
2023-11-13 16:38:10 +0530

    new task

--- Pages/Home.js
+++ Pages/Home.js
@@ -2,26 +2,21 @@ function toggleSidebar() {
     const sidebar = document.getElementById('mySidebar');
     const content = document.getElementById('mainContent');
 
-    // Toggle the width of the sidebar and adjust content margin
     sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
     content.style.marginLeft = sidebar.style.width === '250px' ? '250px' : '0';
 }
 let logoutButton;
 document.addEventListener("DOMContentLoaded", function() {
-    // Check if the user is logged in
     const loggedInUser = localStorage.getItem("loggedInUser");
-    const logoutButton = document.querySelector('#navul li:nth-child(2)'); // Adjust this selector based on your actual structure
+    const logoutButton = document.querySelector('#navul li:nth-child(2)'); 
 
     if (loggedInUser) {
-        // Display the username in the Maincontent div
         console.log("Username retrieved:", loggedInUser);
 
         displayUsernameInMainContent(loggedInUser);
 
-        // Hide login and register buttons
         hideLoginAndRegisterButtons();
     } else {
-        // Show login and register buttons
         showLoginAndRegisterButtons();
     }
     if(!loggedInUser){
@@ -38,13 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
 function displayUsernameInMainContent(username) {
     const mainContent = document.querySelector('.Maincontent');
 
-    // Create a new paragraph element
     const usernameParagraph = document.createElement('p');
 
-    // Set the content of the paragraph with the welcome message and username
     usernameParagraph.innerHTML = `Welcome, ${username}!`;
 
-    // Append the paragraph to the Maincontent div
     mainContent.appendChild(usernameParagraph);
 }
 
@@ -64,17 +56,15 @@ function hideLoginAndRegisterButtons() {
 function showLoginAndRegisterButtons() {
     const loginButton = document.querySelector('#navul li:nth-child(1)');
     const registerButton = document.querySelector('#navul li:nth-child(3)');
-    const logoutButton = document.querySelector('#navul li:nth-child(2)'); // Adjust this selector based on your actual structure
+    const logoutButton = document.querySelector('#navul li:nth-child(2)'); 
 
     loginButton.style.display = 'block';
     registerButton.style.display = 'block';
 }
 
 function logout() {
-    // Clear user data from local storage
     localStorage.removeItem("loggedInUser");
 
-    // Redirect to the login page
     window.location.href = '../index.html';
 }
 
--- index.js
+++ index.js
@@ -2,10 +2,34 @@ function toggleSidebar() {
     const sidebar = document.getElementById('mySidebar');
     const content = document.getElementById('mainContent');
 
-    // Toggle the width of the sidebar and adjust content margin
     sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
     content.style.marginLeft = sidebar.style.width === '250px' ? '250px' : '0';
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.getElementById('mySidebar');
+    const content = document.getElementById('mainContent');
+
+    sidebar.style.width = '0';
+    content.style.marginLeft = '0';
+
+    document.getElementById('toggle-btn').addEventListener('click', function () {
+        sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
+        content.style.marginLeft = sidebar.style.width === '250px' ? '250px' : '0';
+    });
+
+    document.addEventListener('click', function (event) {
+        const isClickInside = sidebar.contains(event.target) || document.getElementById('toggle-btn').contains(event.target);
+
+        if (!isClickInside) {
+            sidebar.style.width = '0';
+            content.style.marginLeft = '0';
+        }
+    });
+});
+
+
+// registration and login forms javascript 
 function showRegistrationForm() {
     document.getElementById('login-container').style.display = 'none';
     document.getElementById('registration-container').style.display = 'block';
@@ -21,7 +45,6 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Check if the user exists in local storage
     const storedUser = localStorage.getItem(username);
 console.log(username);
     if (storedUser) {
@@ -45,16 +68,61 @@ document.getElementById('registration-form').addEventListener('submit', function
     const newUsername = document.getElementById('new-username').value;
     const newPassword = document.getElementById('new-password').value;
     const securityQuestion = document.getElementById('security-question').value;
+    const securityAnswer = document.getElementById('registration-security-answer').value; 
 
     const newUser = {
         username: newUsername,
         password: newPassword,
         securityQuestion: securityQuestion,
+        securityAnswer: securityAnswer,
     };
+    localStorage.setItem(newUsername + '_securityAnswer', securityAnswer);
+    console.log(`Security answer saved for ${newUsername}: ${securityAnswer}`); // Added line
+
 
     localStorage.setItem(newUsername, JSON.stringify(newUser));
     alert('Registration successful!');
 
-    // Redirect to the login form after successful registration
     showLoginForm();
 });
+
+
+function showForgotPasswordForm() {
+    document.getElementById('login-container').style.display = 'none';
+    document.getElementById('registration-container').style.display = 'none';
+
+    document.getElementById('forgotPasswordForm').style.display = 'block';
+}
+
+
+function recoverPassword() {
+    var username = document.getElementById('forgotUsername').value;
+    var securityAnswer = document.getElementById('security-answer').value;
+    console.log(securityAnswer);
+    console.log(username);
+
+    if (!username || !securityAnswer) {
+        alert('Please enter both username and security answer.');
+        return;
+    }
+
+    var storedSecurityAnswer = localStorage.getItem(username + '_securityAnswer');
+    console.log(`Stored security answer for ${username}: ${storedSecurityAnswer}`); 
+    if (securityAnswer === storedSecurityAnswer) {
+        var newPassword = prompt('Enter a new password:');
+
+        if (newPassword) {
+            localStorage.setItem(username + '_password', newPassword);
+            alert('Password reset successful!');
+        } else {
+            alert('Invalid password. Please try again.');
+        }
+    } else {
+        alert('Incorrect security answer. Please try again.');
+    }
+
+    document.getElementById('forgotUsername').value = '';
+    document.getElementById('security-answer').value = '';
+    document.getElementById('forgotPasswordForm').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
